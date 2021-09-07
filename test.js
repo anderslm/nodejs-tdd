ccfe094f89f93b0096190faea8b0b0e4dc505873
Anders Marchsteiner <anders.marchsteiner@lyse.no>
2021-09-07 08:21:21 +0200

    Initial template

---
+++ test.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const expect = require('chai').expect
+const GameOfLife = require('./gof')
+
+describe('Game of Life', () => {
+  describe('"start"', () => {
+      it('should be able to start', () => {
+        expect(User.start).to.be.a('function')
+      })
+   })
+})
